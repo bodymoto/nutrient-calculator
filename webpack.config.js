872966f5e08614607ebc39dc87cfa76e7f384df5
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-22 11:18:20 -0500

    configurations for webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+    },
+  watch: true,
+  target: "web",
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "ie 11" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
