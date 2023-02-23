81f94e5f2a1a95aedda23b73cacc0d02ea1d9fc1
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-23 10:31:48 -0500

    new element init

---
+++ components/elect-element/target-element/target-element.js
@@ -0,0 +1,31 @@
+import {LitElement, html, css} from 'lit';
+
+export class TargetElement extends LitElement {
+	static properties = {}
+
+	constructor() {
+		super();
+	}
+
+	static styles = css`
+	`;
+
+	async handleClick() {
+		const options = {
+			detail: {	target: this.name },
+			bubbles: true,
+			composed: true
+		};
+
+    await this.updateComplete;
+		this.dispatchEvent(new CustomEvent('click-subtract', options));
+	}
+
+	render() {
+		return html`
+			<p @click=${this.handleClick}>elect-element is here!</p>
+		`;
+	}
+}
+
+customElements.define('target-element', TargetElement);
