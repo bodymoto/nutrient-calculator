2259916b74b239c3e80b5af315b68197ccdebee4
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-23 06:02:23 -0500

    added data for testing

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const data = [
+	{
+		"src": "../components/option-element/img/banana.png",
+	  "group": 'fruit',
+	  "name": 'banana'
+  },
+  {
+  	"src": "../components/option-element/img/avocado.jpg",
+    "group": 'fruit',
+    "name": 'avocado'
+  }
+];
