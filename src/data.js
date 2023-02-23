360c3ec20223be7bfd740e9a1755a152ab04e986
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-23 09:42:55 -0500

    new key count for additional tests

--- src/data.js
+++ src/data.js
@@ -2,11 +2,13 @@ export const data = [
 	{
 		"src": "../components/option-element/img/banana.png",
 	  "group": 'fruit',
-	  "name": 'banana'
+	  "name": 'banana',
+	  "count": 0
   },
   {
   	"src": "../components/option-element/img/avocado.jpg",
     "group": 'fruit',
-    "name": 'avocado'
+    "name": 'avocado',
+    "count": 0
   }
 ];
