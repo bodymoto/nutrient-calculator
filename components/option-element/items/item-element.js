e884b71913f47c57548c4a59662b5e4c67bdce3e
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-23 06:07:33 -0500

    adding properties from parent and dispatching click event - testing

--- components/option-element/items/item-element.js
+++ components/option-element/items/item-element.js
@@ -2,17 +2,23 @@ import {LitElement, html, css} from 'lit';
 
 export class ItemElement extends LitElement {
 	static properties = {
+		data: { type: Object },
+		src: { type: String },
+		alt: { type: String },
 		count: { type: Number }
 	}
 
 	constructor() {
 		super();
-		this.count = 1;
+		this.data = {};
+
+		this.src = '';
+		this.alt = '';
+		this.count = 0;
 	}
 
 	static styles = css`
 		:host {
-			position: relative;
 			display: flex;
 			align-content: center;
 			justify-content: space-evenly;
@@ -23,16 +29,22 @@ export class ItemElement extends LitElement {
 		img {
 			width: 82px;
 		}
-		p {
-			position: absolute;
-			font-size: 20px;
-		}
 	`;
+
+	async handleClick() {
+		const options = {
+			detail: {	filter: this },
+			bubbles: true,
+			composed: true
+		};
+
+    await this.updateComplete;
+		this.dispatchEvent(new CustomEvent('click-event', options));
+	}
 
 	render() {
 		return html`
-			<img src="../components/option-element/img/banana.png" alt="" />
-			<p>${this.count}</p>
+			<img @click=${this.handleClick} src=${this.src} alt=${this.alt} />
 		`;
 	}
 }
