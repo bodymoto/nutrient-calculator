0bc28ff294f36afa8c1f040516f5227120a4bfe2
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-23 06:03:22 -0500

    added data to body element

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { BodyElement } from '../components/body-element.js';
+import { data } from './data.js';
 
 const app = document.createElement('body-element');
+
+app.data = data;
 
 document.body.appendChild(app);
