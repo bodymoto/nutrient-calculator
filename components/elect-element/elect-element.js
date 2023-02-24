443e12c98cdff020d187044a67db46c8893d58a0
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-24 04:58:18 -0500

    functionality added to see incoming data and compare with existing data to determine if its missing - setup for adding elements dynamically via click events

--- components/elect-element/elect-element.js
+++ components/elect-element/elect-element.js
@@ -3,25 +3,44 @@ import {TargetElement} from './target-element/target-element.js';
 
 export class ElectElement extends LitElement {
 	static properties = {
-		selected: { type: Object }
+		electData: { type: Array },
+		storage: { type: Array }
 	}
 
 	constructor() {
 		super();
 
-		this.selected = {};
+		this.electData = []; // (1) [{...}]
+		this.storage = [];
+
+		this.addEventListener('click-subtract', (event) => {
+			// do something..
+		});
 	}
 
 	static styles = css`
 		:host {
 			margin: 20px;
 			border: 1px solid black;
+			height: 200px;
 		}
 	`;
 
+	willUpdate(changedProperties) {
+		if (changedProperties.has('electData')) {
+			if (!this.electData.length) {
+				return;
+			}
+
+			if (!this.storage.includes(this.electData[0]))
+				this.storage.push(this.electData[0]); // (2) [{...}, {...}]
+				console.log(this.storage);
+		}
+	}
+
 	render() {
 		return html`
-			<target-element></target-element>
+		<target-element></target-element>
 		`;
 	}
 }
