f73519b6e8017d00ee3cc106c1cb47feb2e1515b
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-02-25 04:35:06 -0500

    building out search functionality - wip

--- components/filter-element/filter-element.js
+++ components/filter-element/filter-element.js
@@ -1,10 +1,44 @@
 import {LitElement, html, css} from 'lit';
 
 export class FilterElement extends LitElement {
-	static properties = {}
+	static properties = {
+		filterData: { type: Array }
+	}
 
 	constructor() {
 		super();
+
+		this.filterData = [];
+		this.input = [];
+		this.search = '';
+
+		this.addEventListener('input', (event) => {
+			if (event.data == null) {
+				this.input.pop();
+			} else {
+				this.input.push(event.data);
+			}
+			this.search = this.input.join('');
+
+			let names = [];
+			this.filterData.map((object) => {
+				names.push(object.name);
+			});
+
+			names.map((name) => {
+				this.search.split(" ").map((word) => {
+					if(name.indexOf(word) != -1) {
+						// name = banana
+						let bozo = this.filterData.filter((object) => {
+							object.name !== name;
+						})
+						console.log(bozo);
+						this.filterData = [...bozo];
+					}
+				})
+			})
+
+		});
 	}
 
 	static styles = css`
@@ -14,9 +48,22 @@ export class FilterElement extends LitElement {
 		}
 	`;
 
+	willUpdate(changedProperties) {
+		console.log(changedProperties);
+	}
+
 	render() {
 		return html`
-			<p>filter-element is here!</p>
+			<input autocomplete="off" type="search" />
+			<ul class="test">
+				${
+					this.filterData.map((object) => {
+						return html`
+						<li>${object.name}</li>
+						`
+					})
+				}
+			</ul>
 		`;
 	}
 }
