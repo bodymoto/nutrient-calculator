31eb8f67654eb32c34b5ba3a4a033433976517c1
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-03-01 05:55:56 -0500

    updated click event info - aligned with bug fix

--- components/elect-element/target-element/target-element.js
+++ components/elect-element/target-element/target-element.js
@@ -2,24 +2,28 @@ import {LitElement, html, css} from 'lit';
 
 export class TargetElement extends LitElement {
 	static properties = {
-		element: { type: Object }
+		element: { type: Object },
+		name: { type: String },
+		count: { type: Number }
 	}
 
 	constructor() {
 		super();
 
 		this.element = {};
+		this.name = '';
+		this.count = null;
 	}
 
 	static styles = css`
-	 li {
-	 	margin: 2px;
-	 }
+		 li {
+		 	margin: 2px;
+		 }
 	`;
 
 	async handleClick() {
 		const options = {
-			detail: {	element: this.element },
+			detail: {	name: this.name },
 			bubbles: true,
 			composed: true
 		};
@@ -29,13 +33,16 @@ export class TargetElement extends LitElement {
 	}
 
 	willUpdate(changedProperties) {
-		console.log(changedProperties);
-		// here???
+		if (changedProperties.has('element')) {
+			console.log(this.shadowRoot);
+		}
+		this.name = this.element.name;
+		this.count = this.element.count;
 	}
 
 	render() {
 		return html`
-			<li @click=${this.handleClick}>${this.element.name}, x${this.element.count}</li>
+			<div @click=${this.handleClick}>${this.name}, x${this.count}</div>
 		`;
 	}
 }
