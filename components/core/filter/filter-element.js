ce94e26f35812460be643bcc91a0198c5e1d7773
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-03-02 11:54:09 -0500

    better naming schema

--- components/core/filter/filter-element.js
+++ components/core/filter/filter-element.js
@@ -2,18 +2,6 @@ import {LitElement, html, css} from 'lit';
 import {FilterByElement} from './filter-by/filter-by-element.js';
 
 export class FilterElement extends LitElement {
-	static properties = {
-		filterData: { type: Array }
-	}
-
-	constructor() {
-		super();
-
-		this.filterData = [];
-	}
-
-	// search by object.group
-
 	static styles = css`
 		:host {
 			margin: 10px;
@@ -21,12 +9,18 @@ export class FilterElement extends LitElement {
 		}
 	`;
 
+	constructor() {
+		super();
+
+		this.data = [];
+	}
+
 	render() {
 		return html`
 			${
-				this.filterData.map((object) => {
+				this.data.map((object) => {
 					return html`
-						<filter-by-element name=${object.group}></filter-by-element>
+						<filter-by-element group=${object.group}></filter-by-element>
 					`
 				})
 			}
