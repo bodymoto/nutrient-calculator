0bad32e7b7ca9ab730a4ef11448c6a27758a9317
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-03-03 01:51:44 -0500

    value added to aid in filtering options

--- src/data.js
+++ src/data.js
@@ -4,13 +4,15 @@ export const data = [
 	  "group": 'fruit',
 	  "name": 'banana',
 	  "count": 0,
-	  "carbs": 12
+	  "carbs": 12,
+	  "checked": false
   },
   {
   	"src": "./src/img/avocado.jpg",
     "group": 'vegetable',
     "name": 'avocado',
     "count": 0,
-    "carbs": 7
+    "carbs": 7,
+    "checked": false
   }
 ];
