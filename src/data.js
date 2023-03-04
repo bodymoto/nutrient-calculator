5aa5af2c62d6f8c74c4578d381530a21f5e3246d
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-03-04 01:34:10 -0500

    expanding the dataset for additional testing

--- src/data.js
+++ src/data.js
@@ -8,11 +8,155 @@ export const data = [
 	  "checked": false
   },
   {
+		"src": "./src/img/banana.png",
+	  "group": 'fruit',
+	  "name": 'strawberry',
+	  "count": 0,
+	  "carbs": 0.922,
+	  "checked": false
+  },
+  {
   	"src": "./src/img/avocado.jpg",
-    "group": 'vegetable',
+    "group": 'fruit',
     "name": 'avocado',
     "count": 0,
-    "carbs": 7,
+    "carbs": 11.8,
     "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'vegetable',
+	  "name": 'tomatoe',
+	  "count": 0,
+	  "carbs": 7.08,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'vegetable',
+	  "name": 'broccoli',
+	  "count": 0,
+	  "carbs": 10,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'vegetable',
+	  "name": 'cucumber',
+	  "count": 0,
+	  "carbs": 10.9,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'meat',
+	  "name": 'tenderloin steak',
+	  "count": 0,
+	  "carbs": 0,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'meat',
+	  "name": 'chicken breast',
+	  "count": 0,
+	  "carbs": 0.35,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'fish',
+	  "name": 'atlantic cod',
+	  "count": 0,
+	  "carbs": 0,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'fish',
+	  "name": 'atlantic salmon',
+	  "count": 0,
+	  "carbs": 0,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'fish',
+	  "name": 'tilapia',
+	  "count": 0,
+	  "carbs": 0,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'shellfish',
+	  "name": 'blue crab',
+	  "count": 0,
+	  "carbs": 0.008,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'shellfish',
+	  "name": 'lobster',
+	  "count": 0,
+	  "carbs": 0,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'shellfish',
+	  "name": 'blue mussel',
+	  "count": 0,
+	  "carbs": 0.59,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'dairy',
+	  "name": 'egg',
+	  "count": 0,
+	  "carbs": 0.36,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'dairy',
+	  "name": 'plain yogurt',
+	  "count": 0,
+	  "carbs": 16,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'dairy',
+	  "name": 'cottage cheese',
+	  "count": 0,
+	  "carbs": 6.68,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'kernels',
+	  "name": 'almond, unsalted',
+	  "count": 0,
+	  "carbs": 0.245,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'kernels',
+	  "name": 'peanuts, 1 cup',
+	  "count": 0,
+	  "carbs": 23.5,
+	  "checked": false
+  },
+  {
+		"src": "./src/img/banana.png",
+	  "group": 'kernels',
+	  "name": 'pistachio',
+	  "count": 0,
+	  "carbs": 0.19,
+	  "checked": false
   }
 ];
