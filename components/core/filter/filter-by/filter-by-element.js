f5cd2ed1c8fc108e376db18e750e98b9fa4f8830
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-03-07 09:45:22 -0500

    styling to label and checkbox

--- components/core/filter/filter-by/filter-by-element.js
+++ components/core/filter/filter-by/filter-by-element.js
@@ -12,6 +12,7 @@ export class FilterByElement extends LitElement {
 			display: flex;
 			flex-wrap: wrap;
 			align-items: center;
+			transition: 500ms;
 		}
 
 		label,
@@ -21,21 +22,33 @@ export class FilterByElement extends LitElement {
 		}
 
 		input[type="checkbox"] {
-			appearance: none;
-			background-color: white;
-			width: 16px;
-			height: 16px;
-			border: 1px solid #FFFF99;
-			border-radius: 2px;
+      appearance: none;
+		  background-color: #fff;
+			width: 32px;
+			height: 32px;
+			border: 2px solid black;
+		  transition: 300ms;
+		}
+
+		input[type="checkbox"]:checked {
+			box-shadow: inset 0 0 0 6px white, inset 0 0 0 16px black;
 		}
 
 		label {
-			padding: 6px;
-			padding-left: 0;
-			font-size: 16px;
+			display: flex;
+			align-items: center;
+			text-shadow: -1px 2px 1px black;
+			height: 32px;
+			padding: 0 6px;
+			margin: 4px 6px;
+			font-size: 20px;
 			font-weight: 700;
 			color: #FFFF99;
 			font-family: sans-serif;
+		}
+
+		label:active {
+			filter: brightness(125%);
 		}
 	`;
 
@@ -81,7 +94,7 @@ export class FilterByElement extends LitElement {
 
 	render() {
 		return html`
-			<input style="background-color: ${this.style}" @change=${this.handleChecked} type="checkbox" ?checked=${this.checked} group=${this.group}>
+			<input @change=${this.handleChecked} type="checkbox" ?checked=${this.checked} group=${this.group}>
 			<label style="background-color: ${this.style}" @click=${this.handleClick} for=${this.group}>${this.group.toUpperCase()}</label>
 		`;
 	}
