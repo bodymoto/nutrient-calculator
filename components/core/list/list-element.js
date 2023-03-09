3ee2a355ab81f0e20170fc5ff42c56f8426722c9
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-03-09 06:31:34 -0500

    styling

--- components/core/list/list-element.js
+++ components/core/list/list-element.js
@@ -3,11 +3,42 @@ import {ItemElement} from './item/item-element.js';
 
 export class ListElement extends LitElement {
 	static styles = css`
+		* {
+			padding: 0;
+			margin: 0;
+			border-sizing: border-box;
+		}
+
 		:host {
+			margin: 10px;
+		}
+
+		.item {
+			font-family: Trebuchet MS;
+			border: 1px solid black;
 			height: 140px;
 			overflow: auto;
-			margin: 10px;
-			border: 1px solid black;
+		}
+
+		.button {
+			display: flex;
+			align-content: center;
+			justify-content: center;
+		}
+
+		button {
+			text-transform: uppercase;
+			font-family: Trebuchet MS;
+			font-weight: 700;
+			letter-spacing: 1px;
+			padding: 10px 32px;
+			margin: 5px;
+			border-radius: 10px;
+			background: linear-gradient(0.55turn, 
+				rgba(150, 206, 180, 0.8), 
+				rgba(255, 238, 173, 0.8), 
+				rgba(255, 204, 92, 0.8), 
+				rgba(255, 111, 105, 0.8));
 		}
 	`;
 
@@ -41,18 +72,21 @@ export class ListElement extends LitElement {
 
 	render() {
 		return html`
+		<div class="item">
+		${this.data.map(
+			(value) => {
+				if (value.count <= 0) {
+					return;
+				}
+				return html`
+					<item-element .value=${value}></item-element>
+				`
+			})
+		}
+		</div>
+		<div class="button">
 			<button @click=${this.handleClick}>Clear all</button>
-
-			${this.data.map(
-				(value) => {
-					if (value.count <= 0) {
-						return;
-					}
-					return html`
-						<item-element .value=${value}></item-element>
-					`
-				})
-			}
+		</div>
 		`;
 	}
 }
