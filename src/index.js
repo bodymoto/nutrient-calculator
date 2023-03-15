891f5bf7050a5ff692cb6c9c7189b616731b9782
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-03-15 13:23:33 +0700

    apped new component to document

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import { HeaderElement } from '../components/header/header-element.js';
 import { CoreElement } from '../components/core/core-element.js';
 import { data } from './data.js';
 
+const header = document.createElement('header-element');
 const app = document.createElement('core-element');
 
 app.data = data;
 
+document.body.appendChild(header);
 document.body.appendChild(app);
