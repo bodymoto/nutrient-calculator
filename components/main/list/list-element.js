9f1abe8e5548d33a820ec9b3c6d153f2c949527c
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-03-17 12:00:06 +0700

    styling: sizing + colors

--- components/main/list/list-element.js
+++ components/main/list/list-element.js
@@ -43,24 +43,19 @@ export class ListElement extends LitElement {
 		}
 
 		button {
-
 			border: none;
 			cursor: pointer;
 			text-transform: uppercase;
 			font-weight: 700;
 			padding: 16px 16px;
-			border: 1px solid black;
 			border-radius: 10px;
-			background: linear-gradient(0.55turn, 
-				rgba(150, 206, 180, 0.8), 
-				rgba(255, 238, 173, 0.8), 
-				rgba(255, 204, 92, 0.8), 
-				rgba(255, 111, 105, 0.8));
-			transition-duration: 300ms;
+			background: #ff6f69;
+			transition-duration: 400ms;
 		}
 
 		button:active {
 			filter: brightness(140%);
+			border-radius: 20px;
 		}
 	`;
 
