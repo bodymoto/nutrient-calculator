e886dd97695a26e5524faad2d25d02a852fc278d
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-03-17 12:46:56 +0700

    appending footer to body

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import { HeaderElement } from '../components/header/header-element.js';
-import { CoreElement } from '../components/main/main-element.js';
+import { MainElement } from '../components/main/main-element.js';
+import { FooterElement } from '../components/footer/footer-element.js';
 import { data } from './data.js';
 
 const header = document.createElement('header-element');
 const main = document.createElement('main-element');
+const footer = document.createElement('footer-element');
 
 main.data = data;
 
 document.body.appendChild(header);
 document.body.appendChild(main);
+// document.body.appendChild(footer);
