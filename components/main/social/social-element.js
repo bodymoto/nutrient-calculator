f9a7906335af0384c9d5bf2b9ace4fa4478ca347
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-03-17 12:01:14 +0700

    copywrite drafting

--- components/main/social/social-element.js
+++ components/main/social/social-element.js
@@ -9,7 +9,9 @@ export class SocialElement extends LitElement {
 		}
 
 		:host {
+			margin: 5px;
 			height: 400px;
+			border: 1px solid black;
 		}
 	`;
 
@@ -19,7 +21,15 @@ export class SocialElement extends LitElement {
 
 	render() {
 		return html`
-			<p>SOCIAL ELEMENT</p>
+			<p>A special message to everyone who made it here. We don't know how, but you did it. You are the first to spelunk this phat dieting tech. We want to do so much more than what you see here today, but we need your help to survive! Follow us, tell us what's working, what's missing, and what's up...</p>
+
+			<p>Were on Reddit</p>
+			<img width="78" height="78" src="../../img/reddit-circle-onwhite.svg" alt="reddit logo" />
+
+			<p>Follow on Twitter</p>
+			<img width="52" height="52" src="../../img/twitter-logo-blue.svg" alt="twitter logo" />
+
+			<p>Send a friendly email:</p>
 		`;
 	}
 }
